69ecc4b382385712c0536e11e3954c4ad33b3327
Ryan Capouellez <rjc8237@nyu.edu>
2025-01-11 18:04:16 -0500

    Removed igl change

--- cmake/libigl.cmake
+++ cmake/libigl.cmake
@@ -7,7 +7,7 @@ include(FetchContent)
   libigl
   SYSTEM
   GIT_REPOSITORY https://github.com/libigl/libigl.git
-  GIT_TAG 5067c8b7eb85343af8f53cdeb0bfd1957ad59e04
-  # GIT_TAG v2.4.0 original: changed for boost fix
+  #GIT_TAG 5067c8b7eb85343af8f53cdeb0bfd1957ad59e04
+  GIT_TAG v2.4.0
 )
 FetchContent_MakeAvailable(libigl)
