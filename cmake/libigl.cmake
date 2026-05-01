9d99c5c0211434c6773654b0e0e4ca0825378a4d
Ryan Capouellez <rjc8237@nyu.edu>
2026-05-01 14:31:40 -0400

    Changed libigl source

--- cmake/libigl.cmake
+++ cmake/libigl.cmake
@@ -6,8 +6,9 @@ include(FetchContent)
   FetchContent_Declare(
   libigl
   SYSTEM
-  GIT_REPOSITORY https://github.com/libigl/libigl.git
+  GIT_REPOSITORY https://github.com/rjc8237/libigl.git
   #GIT_TAG 5067c8b7eb85343af8f53cdeb0bfd1957ad59e04
-  GIT_TAG v2.4.0
+  #GIT_TAG v2.4.0
+  GIT_TAG penner
 )
 FetchContent_MakeAvailable(libigl)
